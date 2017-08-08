8eac66d9023bf3905e8f831b68d59fe2a77a5783
Suraj Raval <raval.suraj@gmail.com>
2017-08-09 02:52:17 +0530

    Added Password Hashing to the Account model

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 // Get the packages we need
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var Account = require('./models/account');
+var Customer = require('./models/customer');
+var apiRouter = require('./routes/apiRouter');
+var apiController = require('./controllers/apiController');
+
+//Connect to the Database
 mongoose.connect('mongodb://localhost:27017/srdesignstudio');
+
 // Create our Express application
 var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
@@ -13,9 +24,8 @@ var router = express.Router();
 
 // Initial dummy route for testing
 // http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the API for Sonal Raval design Studio' });
-});
+
+apiRouter(app);
 
 // Register all our routes with /api
 app.use('/api', router);
