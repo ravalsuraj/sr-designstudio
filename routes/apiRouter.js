d61dc54aa99f19b78b45cb9267594103c9f6359b
Suraj Raval <raval.suraj@gmail.com>
2017-08-20 20:33:31 +0530

    Added Index View (WIP)

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,34 +1,40 @@
 'use strict';
+var express = require('express');
+var router = express.Router();
 
-module.exports = function(app) {
-  var crudControl = require('../controllers/crudControl');
-  var authControl = require('../controllers/authControl');
+var crudControl = require('../controllers/crudControl');
+var authControl = require('../controllers/authControl');
 
+router.use(function timeLog (req, res, next) {
+  next()
+});
 // crudControl API Routes
 
-  app.route('/api')
+  router.route('/')
     .get(crudControl.send_greeting);
 
 //API Routes for Accounts
 
-  app.route('/api/accounts')
+  router.route('/accounts')
   .post(crudControl.create_account)
   //TODO: do not include get method in production API
   .get(authControl.isValidAccount, crudControl.read_accounts);
 
-  app.route('/api/accounts/:accountId')
+  router.route('/accounts/:accountId')
     //TODO: do not include get method in production API
     .get(authControl.isValidAccount, crudControl.read_account)
     .put(authControl.isValidAccount, crudControl.update_account)
     .delete(authControl.isValidAccount, crudControl.delete_account);
 
 //API Routes for Customers
-  app.route('/api/customers')
+  router.route('/customers')
     .post(authControl.isValidAccount, crudControl.create_customer)
     .get(authControl.isValidAccount, crudControl.read_customers);
 
-  app.route('/api/customers/:customerId')
+  router.route('/customers/:customerId')
     .get(authControl.isValidAccount, crudControl.read_customer)
     .put(authControl.isValidAccount, crudControl.update_customer)
     .delete(authControl.isValidAccount, crudControl.delete_customer);
-};
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var pug = require('pug');
+var path = require('path');
 
 var Account = require('./models/account');
 var Customer = require('./models/customer');
 var apiRouter = require('./routes/apiRouter');
+var webRouter = require('./routes/webRouter');
+
 var crudControl = require('./controllers/crudControl');
 
 //Connect to the Database
@@ -13,8 +18,13 @@ mongoose.connect('mongodb://localhost:27017/srdesignstudio');
 
 // Create our Express application
 var app = express();
-
+app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({extended:true}));
+
+// view engine and resource setup
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
@@ -25,11 +35,14 @@ var router = express.Router();
 // Initial dummy route for testing
 // http://localhost:3000/api
 
-apiRouter(app);
+//apiRouter(app);
 
 // Register all our routes with /api
-app.use('/api', router);
+app.use('/api', apiRouter);
+app.use('/', webRouter);
 
 // Start the server
 app.listen(port);
 console.log('Sonal Raval Design Studio API running on port ' + port);
+
+//
