00b602f48cf3c3deef6f7f2af06f7f97f7bb96fb
Suraj Raval <raval.suraj@gmail.com>
2017-08-25 16:00:46 +0530

    Added full page scroll and refined UI

--- public/js/indexJS.js
+++ public/js/indexJS.js
@@ -9,14 +9,15 @@ $(document).ready(function() {
     items: 1,
     center: true,
     startPosition: 1,
-    autoplay: false,
+    autoplay: true,
     smartSpeed: 500,
     paginationSpeed: 300,
     singleItem: true,
     responsive: {
       0: {
-        margin: 10,
-        stagePadding: 10,
+        margin: 0,
+        stagePadding: 0,
+        dots: false,
       },
 
       768: {
@@ -25,42 +26,48 @@ $(document).ready(function() {
       }
     }
   })
-  //$('a').smoothScroll();
+
+  //Call the fullPage.JS plugin. Allows snap-scrolling full page at a time
+  $('#fullpage').fullpage({
+    verticalCentered:false,
+    //sectionsColor:'none',
+    paddingTop:'10vh'
+
+  });
 
 
-//Smooth Scroll and Automatic setting of active Navbar items
-$('.top-nav a').on('click', function() {
+  //Smooth hyperlink redirection and Automatic setting Navbar items as active
+  $('.top-nav a').on('click', function() {
 
     var scrollAnchor = $(this).attr('data-scroll'),
-        scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+      scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
 
     $('body,html').animate({
-        scrollTop: scrollPoint
+      scrollTop: scrollPoint
     }, 500);
 
     return false;
 
-})
+  })
 
-
-$(window).scroll(function() {
+  $(window).scroll(function() {
     var windscroll = $(window).scrollTop();
     if (windscroll >= 100) {
-        $('.top-nav').addClass('fixed');
-        $('.wrapper section').each(function(i) {
-            if ($(this).position().top <= windscroll + 100) {
-                $('.top-nav li.active').removeClass('active');
-                $('.top-nav li').eq(i).addClass('active');
-            }
-        });
+      $('.top-nav').addClass('fixed');
+      $('.wrapper section').each(function(i) {
+        if ($(this).position().top <= windscroll + 100) {
+          $('.top-nav li.active').removeClass('active');
+          $('.top-nav li').eq(i).addClass('active');
+        }
+      });
 
     } else {
 
-        $('.top-nav').removeClass('fixed');
-        $('.top-nav li.active').removeClass('active');
-        $('.top-nav li:first').addClass('active');
+      $('.top-nav').removeClass('fixed');
+      $('.top-nav li.active').removeClass('active');
+      $('.top-nav li:first').addClass('active');
     }
+  }).scroll();
 
-}).scroll();
 
 });
